eaf40c9b285ed9568ce890860fa2a620dc5cbb26
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:21:41 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,82 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Both client and server release _________ connection after a page has been transferred",
+    answer: "Pointer",
+    options: [
+      "AV003-A",
+      "AV110-D",
+      "AV005-B",
+      "AV112-D"
+    ]
+  },
+  {
+    numb: 2,
+    question: "In HTML, the tags that tell the browser how to display the page.",
+    answer: "Matlab",
+    options: [
+      "AV002-B",
+      "AV109-A",
+      "AV006-C",
+      "AV113-A"
+    ]
+  },
+  {
+    numb: 3,
+    question: "FTP stands for?",
+    answer: "Java",
+    options: [
+      "AV001-D",
+      "AV108-A",
+      "AV007-B",
+      "AV114-D"
+    ]
+  },
+  {
+    numb: 4,
+    question: "A _________ partitions a web browser window so that multiple web documents can be displayed simultaneously.",
+    answer: "Robot",
+    options: [
+      "AV010-D",
+      "AV103-B",
+      "AV012-C",
+      "AV105-A"
+    ]
+  },
+  {
+    numb: 5,
+    question: "A program that can execute high-level language programs.",
+    answer: "Drive",
+    options: [
+      "AV009-C",
+      "AV102-B",
+      "AV013-D",
+      "AV106-D"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Source program is compiled to an intermediate form called ___________",
+    answer: "Program",
+    options: [
+      "AV008-B",
+      "AV101-A",
+      "AV014-B",
+      "AV107-D"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "JIT stands for?",
+    answer: "Python",
+    options: [
+      "AV014-A",
+      "AV107-B",
+      "AV008-A",
+      "AV101-D"
+    ]
+  },
+];
